6a4e782f17b6b2678144047932dd35a72a439dff
Adonis33142 <selemtygerml@outlook.com>
2026-06-04 18:27:20 +0800

    Add builder code suffix

--- lib/wagmi.ts
+++ lib/wagmi.ts
@@ -2,8 +2,8 @@ import { createConfig, http, injected } from 'wagmi'
 import { base } from 'wagmi/chains'
 import { coinbaseWallet } from 'wagmi/connectors'
 
-// TODO: Replace this value after Base.dev verification.
-export const BUILDER_CODE_SUFFIX = '' as `0x${string}` | ''
+// Builder code: bc_v2jvcg89
+export const BUILDER_CODE_SUFFIX = '0x62635f76326a76636738390b0080218021802180218021802180218021' as const
 
 export const wagmiConfig = createConfig({
   chains: [base],
