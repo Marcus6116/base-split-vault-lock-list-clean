a878802792ca58f015134cc18ec118b18425f700
Adonis33142 <selemtygerml@outlook.com>
2026-06-05 15:41:38 +0800

    Add Talent project verification tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,10 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
           name="talentapp:project_verification"
           content="c72e1997b2a5f7c751001aaf5fe9122349136a041df6c64c06cd757abb94722c9ac31eeb56c47c32b9d1df209346c1e29836d4403d9167139361adee2266fc4e"
         />
+        <meta
+          name="talentapp:project_verification"
+          content="bbe4d93f308820a001463633fb487f80308a2f077573272387434159623d2668c683205dd0af8b07e27b54fec2fa0fc565ddefe16b4a7ab64a061d132e1af755"
+        />
       </head>
       <body>
         <AppProviders>{children}</AppProviders>
